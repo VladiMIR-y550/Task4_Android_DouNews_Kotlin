6d2063756bcce1fef36bd421a09c5160b74c1177
VladiMIR-y550 <mironenko.vladimir2019@gmail.com>
2022-04-20 22:06:33 +0300

    start new project

---
+++ app/src/main/java/com/example/task4_android_dounews_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.task4_android_dounews_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
