e9549a0246dc2d2e177f243ded67efab3d8d90e9
Vladimir Mironenko <vladimir.mironenko.natife@gmail.com>
2023-04-01 15:52:04 +0300

    added BaseFragment

--- app/src/main/java/com/example/task4_android_dounews_kotlin/screens/news_detail/NewsDetailFragment.kt
+++ app/src/main/java/com/example/task4_android_dounews_kotlin/screens/news_detail/NewsDetailFragment.kt
@@ -10,11 +10,14 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.example.task4_android_dounews_kotlin.R
+import com.example.task4_android_dounews_kotlin.base.BaseFragment
 import com.example.task4_android_dounews_kotlin.databinding.FragmentDetailNewsBinding
 import com.example.task4_android_dounews_kotlin.utils.network.NetworkStatus
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,13 +25,16 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
-class NewsDetailFragment : Fragment() {
+class NewsDetailFragment : BaseFragment<FragmentDetailNewsBinding>() {
 
     private val args by navArgs<NewsDetailFragmentArgs>()
-    private var bindingInternal: FragmentDetailNewsBinding? = null
-    private val binding get() = bindingInternal!!
     private val viewModel: NewsDetailViewModel by viewModels()
     private lateinit var scrollObserver: Observer<Int>
+
+    override val viewBindingProvider: (LayoutInflater, ViewGroup?) -> FragmentDetailNewsBinding =
+        { inflater, container ->
+            DataBindingUtil.inflate(inflater, R.layout.fragment_detail_news, container, false)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,23 +43,14 @@ class NewsDetailFragment : Fragment() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.R)
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        bindingInternal = FragmentDetailNewsBinding.inflate(inflater, container, false)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         scrollObserver = Observer {
             binding.webViewDetailed.scrollTo(0, it)
         }
-
         binding.viewModel = viewModel
-
         webViewSetup()
         checkNetworkStatus()
-        return binding.root
     }
 
     override fun onStart() {
@@ -69,11 +66,6 @@ class NewsDetailFragment : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         viewModel.saveScrollPosition(binding.webViewDetailed.scrollY)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        bindingInternal = null
     }
 
     @SuppressLint("SetJavaScriptEnabled")
--- app/src/main/java/com/example/task4_android_dounews_kotlin/screens/news_list/NewsListFragment.kt
+++ app/src/main/java/com/example/task4_android_dounews_kotlin/screens/news_list/NewsListFragment.kt
@@ -1,16 +1,17 @@
 package com.example.task4_android_dounews_kotlin.screens.news_list
 
 import android.os.Bundle
+import android.provider.ContactsContract.RawContacts.Data
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.example.task4_android_dounews_kotlin.R
+import com.example.task4_android_dounews_kotlin.base.BaseFragment
 import com.example.task4_android_dounews_kotlin.databinding.FragmentNewsListBinding
 import com.example.task4_android_dounews_kotlin.domain.modelsUi.ArticleUi
 import com.example.task4_android_dounews_kotlin.utils.*
@@ -20,9 +21,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
-class NewsListFragment : Fragment() {
-    private var bindingInternal: FragmentNewsListBinding? = null
-    private val binding get() = bindingInternal!!
+class NewsListFragment : BaseFragment<FragmentNewsListBinding>() {
     private val viewModel: NewsListViewModel by viewModels()
     private val pagingAdapter by lazy {
         NewsListAdapter(loadPage = {
@@ -30,19 +29,19 @@ class NewsListFragment : Fragment() {
         })
     }
 
+    override val viewBindingProvider: (LayoutInflater, ViewGroup?) -> FragmentNewsListBinding =
+        { inflater, container ->
+            DataBindingUtil.inflate(inflater, R.layout.fragment_news_list, container, false)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         pagingAdapter.stateRestorationPolicy =
             RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        bindingInternal =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_news_list, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel.articles.observe(viewLifecycleOwner, ::renderState)
         checkNetworkStatus()
 
@@ -55,7 +54,6 @@ class NewsListFragment : Fragment() {
             adapter = pagingAdapter
             this.setHasFixedSize(true)
         }
-        return binding.root
     }
 
     private fun renderState(state: Result<List<ArticleUi>>) {
@@ -83,11 +81,6 @@ class NewsListFragment : Fragment() {
                 ).show()
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        bindingInternal = null
     }
 
     private fun hideAll() {
